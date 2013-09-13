f8e975bf934395abb2c315dd407b67f401fca032
Tom Readings <digitisatom@gmail.com>
2013-09-13 11:46:24 +0100

    subthemes folder moved external to main theme to avoid template conflicts

--- plus/template.php
+++ plus/template.php
@@ -14,6 +14,24 @@ function plus_menu_link($variables) {
   }
 }
 
+function plus_process_page(&$variables) {
+  // display suite is handling page-node titles so that I can have short/long versions
+  if (isset($variables['node']) && $variables['node']->type == 'page'){
+    if (isset($variables['node']->field_longtitle['und'][0]['value'])) {
+      $variables['title'] = $variables['node']->field_longtitle['und'][0]['value'];
+    }
+    if (isset($variables['node']->book) && $variables['node']->book['depth'] < 3) {
+      $variables['title'] = NULL;
+    } 
+  }
+  global $user;
+
+  //drupal_set_message($_SESSION['institution']);
+  //watchdog('bootstrapped', $user->uid.':'. $_SESSION['institution']);
+  //$variables['catalog_link'] = l('Search Catalogue', 'http://voyager.falmouth.ac.uk/', _get_button_attributes());
+
+}
+
 
 // the call to action buttons on home page are just plain menu links
 // they need bootstrap magic
