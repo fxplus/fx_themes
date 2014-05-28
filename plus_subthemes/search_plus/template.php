cb5fc2c43acd463372040b78972a46c95cbcc5ee
Tom Readings <digitisatom@gmail.com>
2014-05-28 11:12:33 +0100

    improve display of search results. service icons etc

--- plus_subthemes/search_plus/template.php
+++ plus_subthemes/search_plus/template.php
@@ -1,69 +1,66 @@
 <?php 
 
-function search_plus_process_page(&$variables) {
-  // display suite is handling page-node titles so that I can have short/long versions
-  if (isset($variables['node']) && $variables['node']->type == 'page'){
-    if (isset($variables['node']->field_longtitle['und'][0]['value'])) {
-      $variables['title'] = $variables['node']->field_longtitle['und'][0]['value'];
-    }
-          // if this is the main book (ie the website not the referencing guide) 
-    if ($variables['node']->book['menu_name'] == 'book-toc-1') {
-      if (isset($variables['node']->book) && $variables['node']->book['depth'] < 3) {
-        $variables['title'] = NULL;
-      }
-    } else {
-      // do not display book title on top level page, as duplicates block title
-      if (isset($variables['node']->book) && $variables['node']->book['depth'] < 2) {
-        $variables['title'] = NULL;
-      }
-    }
+/**
+ * Implements theme_form_element.
+ *
+ * This is done to remove some cruft from search form that gets in the way 
+ * of bootstap search button append
+ */
+function search_plus_form_element($variables) {
+  $element = &$variables['element'];
+  $element += array(
+    '#title_display' => 'before',
+  );
+  // REMOVED WRAPPER DIV AND ALL form- CLASSES
+  $output = '';
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
   }
-}
+  $prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . $element['#field_prefix'] . '</span> ' : '';
+  $suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . $element['#field_suffix'] . '</span>' : '';
 
-function search_plus_menu_link($variables) {
-  if ($variables['element']['#original_link']['menu_name'] == 'menu-home-actions') {
-    $element = $variables['element'];
-    $output = l($element['#title'], $element['#href'], _search_plus_button_attributes($variables['element']['#original_link']));
-    return $output."\n";
-    // return '<li' . drupal_attributes($element['#attributes']) . '>'.$output."</li>\n";
-  } else {
-    $element = $variables['element'];
-    return theme_menu_link($variables);
+  switch ($element['#title_display']) {
+    case 'before':
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+    case 'after':
+      $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
   }
+  if (!empty($element['#description'])) {
+    $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+  }
+  // REMOVED WRAPPER DIV
+  return $output;
 }
-
-function _search_plus_button_attributes($menu_link = FALSE) {
-  if (!$menu_link) {
-    $attributes = array('attributes' => array(
-      'class' => array('btn', 'btn-info', 'btn-large', 'catalog-link'),
-    ));
-    return $attributes;
-  } else {
-    if (isset($menu_link['mlid'])) {
-      // classes to apply to individual menu links
-      switch($menu_link['mlid']) {
-        case '595':
-          $linkclass = array('btn', 'btn-large', 'btn-fxblue', 'catalog-link');
-          break;
-        case '1172':
-          $linkclass = array('btn', 'btn-large', 'btn-fxolive', 'catalog-link');
-          break;
-        default: 
-          $linkclass = array('btn', 'btn-large', 'catalog-link');
-          break;
-      }
-      // classes to apply to every link in a menu
-      switch($menu_link['menu_name']) {
-        case('menu-home-actions'):
-          $menuclass = array('btn', 'btn-large', 'span9', 'pull-right');
-          break;
-        default:
-          $menuclass = array();
-          break;
-      }
-      $classes = array_merge($linkclass, $menuclass);
-      $attributes = array('attributes' => array('class' => $classes));
-    }
+/**
+ * Implements theme_button.
+ *
+ * This is done to make submit a button rather than input
+ */
+function search_plus_button($variables) {
+  $element = $variables['element'];
+  $element['#attributes']['type'] = 'submit';
+  element_set_attributes($element, array('id', 'name', 'value'));
+  $element['#attributes']['class'][] = 'btn';
+  $element['#attributes']['class'][] = 'form-' . $element['#button_type'];
+  if (!empty($element['#attributes']['disabled'])) {
+    $element['#attributes']['class'][] = 'form-button-disabled';
   }
-  return $attributes;
+
+  if ($element['#value'] != 'Search') {
+    $button = '<input' . drupal_attributes($element['#attributes']) . ' />';
+  } else {
+    $button = '<button' . drupal_attributes($element['#attributes']) . '><i class="icon-search"></i></button>';
+  }
+  return $button;
 }
