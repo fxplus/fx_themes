98d81c7a0e1f1afe4e12699c127fb8ed582010f3
Tom Readings <digitisatom@gmail.com>
2015-04-13 16:05:26 +0100

    theme library search block to match open framework site search

--- plus_subthemes/library_plus/template.php
+++ plus_subthemes/library_plus/template.php
@@ -1,5 +1,46 @@
 <?php 
 
+// // add template (file) for fx_searchblock
+// function library_plus_theme() {
+//   dpm('library_plus_theme');
+//   return array( 
+//     'fx_searchblock_block_form' => array(
+//       'render element' => 'form',
+//       // 'path' => drupal_get_path('theme', 'library_plus') . '/templates',
+//       'template' => 'templates/fx-searchblock-block-form',
+//       ),
+//     );
+// }
+
+// add theme function for fx_searchblock
+function library_plus_theme() {
+  return array( 
+    'fx_searchblock_block_form' => array(
+      'render element' => 'form',
+    ),
+  );
+}
+/* 
+ * theme function to customise display of the catalog/search box
+ * (could equally be done via template+preprocess, or via hook_form_alter)
+ */
+function library_plus_fx_searchblock_block_form($variables) { 
+  // removes wrapper div from search query textfield (req for bootstrap or OF)
+  unset($variables['form']['fx_searchblock_form']['#theme_wrappers']);
+  // open framework search block styling
+  $variables['form']['fx_searchblock_form']['#title_display'] = 'invisible';
+  $variables['form']['fx_searchblock_form']['#attributes']['class'][] = 'input-medium search-query';
+  $variables['form']['fx_searchblock_form']['#attributes']['placeholder'] = t('Search the catalogue');
+  $variables['form']['submit']['#attributes']['class'][] = 'btn-search';
+  $variables['form']['submit']['#attributes']['alt'] = t('Search button');
+  // unset($variables['form']['submit']['#value']);    
+  $variables['form']['submit']['#type'] = 'image_button';
+      // $form['submit']['#type'] = 'image_button';
+  $variables['form']['submit']['#src'] = drupal_get_path('theme', 'open_framework') . '/images/searchbutton.png';
+  dpm($variables);
+  return drupal_render_children($variables['form']);
+}
+
 function library_plus_menu_link($variables) {
   if ($variables['element']['#original_link']['menu_name'] == 'menu-home-actions') {
     $element = $variables['element'];
@@ -49,7 +90,7 @@ function _library_plus_button_attributes($menu_link = FALSE) {
       // classes to apply to every link in a menu
       switch($menu_link['menu_name']) {
         case('menu-home-actions'):
-          $menuclass = array('btn', 'btn-large', 'span9', 'pull-right');
+          $menuclass = array('btn', 'btn-large', 'span12', 'pull-right');
           break;
         default:
           $menuclass = array();
@@ -61,3 +102,50 @@ function _library_plus_button_attributes($menu_link = FALSE) {
   }
   return $attributes;
 }
+
+/* Search Form Block (taken from open_framework) */
+function library_plus_preprocess_block(&$variables) {
+  if ($variables['block']->module == 'fx_searchblock') {
+    $variables['attributes_array']['role'] = 'search';
+  }
+}
+
+function library_plus_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'fx_searchblock_block_form') {
+    // for some reason this will not take effect from the theme function (library_plus_fx_searchblock_block_form)
+    $form['submit']['#type'] = 'image_button';
+    // unset($form['fx_searchblock_block_form']['#theme_wrappers']);
+    // $form['fx_searchblock_block_form']['#title_display'] = 'invisible';
+    // $form['fx_searchblock_block_form']['#attributes']['class'][] = 'input-medium search-query';
+    // $form['fx_searchblock_block_form']['#attributes']['placeholder'] = t('Search this site...');
+    // $form['submit']['#attributes']['class'][] = 'btn-search';
+    // $form['submit']['#attributes']['alt'] = t('Search button');
+    // unset($form['actions']['submit']['#value']);    
+    // $form['submit']['#src'] = drupal_get_path('theme', 'open_framework') . '/images/searchbutton.png';
+  }
+}
+
+
+/*
+ * Apply classes to blocks dynamically so that contexts/theme-settings can be altered
+ * by admin to change layout disabled because contrib module
+ * cbc will this, [dev] admin of multiple blocks could be a pain through ui
+ * see cbc_set_block_classes to alter through code
+ * (https://www.drupal.org/project/cbc)
+ */
+// function plus_preprocess_block(&$variables) {
+  // dpm($variables['block']->bid);
+  // dpm($variables);
+  // $blockclasses = array(
+  //   'fx_searchblock-form' => 'span4',
+  //   'menu-menu-quick-links' => 'span4',
+  //   'menu-menu-home-actions' => 'span4',
+  //   );
+  // $classblocks = array_flip($blockclasses);
+  // if ($variables['block']->bid === 'fx_searchblock-form') {
+  //   // dpm($variables);
+  //   $variables['classes_array'][] = 'span4';
+  //   // dpm($variables);
+  //   // do something for this block
+  // }
+// }
