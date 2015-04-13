1a1065ea3ea8796d8ba28a873e6644e9258070dc
Tom Readings <digitisatom@gmail.com>
2015-04-13 16:27:25 +0100

    snippet to administer contextual block classes

---
+++ plus_subthemes/library_plus/dev_resources/cbc_hack.php
@@ -0,0 +1,17 @@
+<?php
+
+$block_context_block = array(
+    'homepage_content' => array(
+        'fx_searchblock-form' => array(
+                'span6',
+            ),
+        'menu-menu-quick-links' => array(
+                'span3',
+            ),
+        'menu-menu-home-actions' => array(
+                'span3',
+            ),
+    ),
+);
+
+$variable_set('contextual_block_class', $block_context_block);
