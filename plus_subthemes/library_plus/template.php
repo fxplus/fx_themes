100057c34e258dcfc9bcc54957aca416833e736a
Tom Readings <digitisatom@gmail.com>
2015-04-22 14:53:17 +0100

    ua-* classes to avoid name conflict with drupal alerts

--- plus_subthemes/library_plus/template.php
+++ plus_subthemes/library_plus/template.php
@@ -182,7 +182,7 @@ function library_plus_preprocess_user_alert(&$vars) {
   $node = $vars['node'];
   // $tag = taxonomy_term_load($node->field_tags['und'][0]['tid']);
   // $vars['importance'] = $tag ? $tag->name : 'alert';
-  $vars['importance'] = $node->field_importance['und'][0]['value'];
+  $vars['importance'] = 'ua-'. $node->field_importance['und'][0]['value'];
   $vars['alert_label'] = variable_get('user_alert_label', 'User Alert');
   $vars['nid'] = $vars['node']->nid;
   // $vars['body'] = $vars['node']->body[$vars['node']->language][0]['value'];
